92ca196a673b99d20e78729b596853df754d88b1
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-07 16:43:57 +0530

    Create variable.tf

---
+++ modules/loadbalancer/variable.tf
@@ -0,0 +1,24 @@
+variable "instance_id" {
+  type = string
+}
+variable "zone" {
+  type = string
+}
+variable "group_name" {
+  type = string
+}
+variable "hc_name" {
+  type = string
+}
+variable "backend_name" {
+  type = string
+}
+variable "url_name" {
+  type = string
+}
+variable "proxy_name" {
+  type = string
+}
+variable "var.rule_name" {
+  type = string
+}
