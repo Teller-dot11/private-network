c49e7ec37b687732af90c29b1bed27a204203dfa
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-07 16:32:36 +0530

    Create main.tf

---
+++ modules/compute/main.tf
@@ -0,0 +1,22 @@
+resource "google_compute_instance" "vm" {
+ name = var.vm_name //"private-web-vm"
+ machine_type = var.machine_type //"e2-micro"
+ zone = var.zone
+
+ boot_disk {
+ initialize_params {
+  image = "debian-cloud/debian-12"
+  }
+ }
+
+ network_interface {
+  subnetwork = var.subnet_id
+ }
+
+ metadata_startup_script = <<-EOF
+ #!/bin/bash
+ apt-get update
+ apt-get install -y apache2
+ echo "Hello from Private VM" > /var/www/html/index.html
+ EOF
+}
