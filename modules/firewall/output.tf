10dd13bfe9fd5420cdd56981e5f373477a2d2d77
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-07 23:30:46 +0530

    Create output.tf

---
+++ modules/firewall/output.tf
@@ -0,0 +1,8 @@
+output "firewall_rules" {
+  description = "Names of firewall rules created by the firewall module."
+  value = [
+    google_compute_firewall.allow_lb_http.name,
+    google_compute_firewall.allow_internal.name,
+    google_compute_firewall.allow_iap_ssh.name
+  ]
+}
