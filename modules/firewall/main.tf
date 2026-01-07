f0f5d88eebc769f33eb1721ee33986e9a266211f
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-07 23:28:28 +0530

    Create main.tf

---
+++ modules/firewall/main.tf
@@ -0,0 +1,63 @@
+resource "google_compute_firewall" "allow_lb_http" {
+  name = "allow-lb-http"
+  network = var.network_name
+
+  direction = "INGRESS"
+  priority = 1000
+
+  source_ranges = var.lb_health_check_ranges
+
+  allow {
+    protocol = "tcp"
+    ports = ["80"]
+  }
+
+  target_tags = var.target_tags
+}
+
+############################################
+# Allow Internal VPC Traffic
+############################################
+resource "google_compute_firewall" "allow_internal" {
+  name = "allow-internal"
+  network = var.network_name
+
+  direction = "INGRESS"
+  priority = 1000
+
+  source_ranges = [var.internal_cidr]
+
+  allow {
+    protocol = "tcp"
+    ports = ["0-65535"]
+  }
+
+  allow {
+    protocol = "udp"
+    ports = ["0-65535"]
+  }
+
+  allow {
+    protocol = "icmp"
+  }
+}
+
+############################################
+# Allow SSH via IAP Only
+############################################
+resource "google_compute_firewall" "allow_iap_ssh" {
+  name = "allow-iap-ssh"
+  network = var.network_name
+
+  direction = "INGRESS"
+  priority = 1000
+
+  source_ranges = var.iap_ssh_ranges
+
+  allow {
+    protocol = "tcp"
+    ports = ["22"]
+  }
+
+  target_tags = var.target_tags
+}
