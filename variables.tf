78b196c726e70270b51f388de14c6b66aa13a328
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-07 22:00:13 +0530

    Update variables.tf
    values

--- variables.tf
+++ variables.tf
@@ -1,9 +1,62 @@
 variable "project_id" {
  type = string
+ default = "strategic-cargo-483609-k0"
 }
 variable "region" {
  type = string
+ default = "us-east1"
 }
 variable "zone" {
  type = string
+ default = "us-east1-b"
 }
+variable "vm_name" {
+ type = string
+ default = "private-web-vm"
+}
+variable "machine_type" {
+ type = string
+ default = "e2-micro"
+}
+variable "group_name" {
+ type = string
+ default = "web-group"
+}
+variable "hc_name" {
+ type = string
+ default = "http-hc"
+}
+variable "backend_name" {
+ type = string
+ default = "web-backend"
+}
+variable "url_name" {
+ type = string
+ default = "url-map"
+}
+variable "proxy_name" {
+ type = string
+ default = "http-proxy"
+}
+variable "rule_name" {
+ type = string
+ default = "http-rule"
+}
+variable "router_name" {
+ type = string
+ default = "nat-router"
+}
+variable "nat_name" {
+ type = string
+ default = "cloud-nat"
+}
+variable "vpc_name" {
+ type = string
+ default = "private-vpc"
+}
+variable "subnet_name" {
+ type = string
+ default = "private-subnet"
+}
+
+hc_name //"http-hc"
