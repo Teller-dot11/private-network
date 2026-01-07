2458ef764daf4f6e9d9614353357f64421169e30
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-07 16:38:51 +0530

    Create main.tf

---
+++ modules/loadbalancer/main.tf
@@ -0,0 +1,45 @@
+resource "google_compute_instance_group" "group" {
+ name = var.group_name //"web-group"
+ zone = var.zone
+ instances = [var.instance_id]
+
+ named_port {
+  name = "http"
+  port = 80
+  }
+}
+
+resource "google_compute_health_check" "hc" {
+ name = hc_name //"http-hc"
+
+ http_health_check {
+  port = 80
+  }
+ }
+
+resource "google_compute_backend_service" "backend" {
+ name = var.backend_name //"web-backend"
+ protocol = "HTTP"
+ port_name = "http"
+ health_checks = [google_compute_health_check.hc.id]
+
+ backend {
+  group = google_compute_instance_group.group.id
+}
+}
+
+resource "google_compute_url_map" "map" {
+ name = var.url_name //"url-map"
+ default_service = google_compute_backend_service.backend.id
+}
+
+resource "google_compute_target_http_proxy" "proxy" {
+ name = var.proxy_name //"http-proxy"
+ url_map = google_compute_url_map.map.id
+}
+
+resource "google_compute_global_forwarding_rule" "rule" {
+ name = var.rule_name //"http-rule"
+ target = google_compute_target_http_proxy.proxy.id
+ port_range = "80"
+}
