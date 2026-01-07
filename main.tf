ef82568ca638fd925aef65bfcd44f79ffc29d154
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-07 22:19:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
 module "network" {
  source = "./modules/network"
+ vpc_name = var.vpc_name
+ subnet_name =  var.subnet_name
  region = var.region
 }
 
 module "nat" {
  source = "./modules/nat"
+ router_name = var.router_name
+ nat_name = var.nat_name
  network_id = module.network.vpc_id
  region = var.region
 }
@@ -17,6 +21,12 @@ module "compute" {
 
 module "loadbalancer" {
  source = "./modules/loadbalancer"
+ group_name = var.group_name
+ hc_name = var.hc_name
+ backend_name = var.backend_name
+ url_name = var.url_name
+ proxy_name = var.proxy_name
+ rule_name = var.rule_name
  instance_id = module.compute.instance_id
  zone = var.zone
 }
