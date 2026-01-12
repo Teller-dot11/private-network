a1b02d5db97c510364ea56cc9515a6feeac9ac34
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-12 15:00:41 +0530

    Update main.tf
    load balancer

--- main.tf
+++ main.tf
@@ -42,3 +42,18 @@ module "certificate" {
   source = "./modules/certificate"
   domain = var.domain
 }
+
+module "loadbalancer" {
+  source = "./modules/loadbalancer"
+
+  group_name    = var.group_name
+  hc_name       = var.hc_name
+  backend_name  = var.backend_name
+  url_name      = var.url_name
+  proxy_name    = var.proxy_name
+  rule_name     = var.rule_name
+  instance_id   = module.compute.instance_id
+  zone          = var.zone
+
+  certificate_id = module.certificate.certificate_id
+}
