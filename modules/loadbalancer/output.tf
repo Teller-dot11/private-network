b0a0212795715bfc25ab435ac0b8caefe6e4e795
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-12 17:37:20 +0530

    Update output.tf

--- modules/loadbalancer/output.tf
+++ modules/loadbalancer/output.tf
@@ -1,3 +1,8 @@
 output "lb_ip" {
  value = google_compute_global_forwarding_rule.rule.ip_address
 }
+
+output "https_lb_ip" {
+  description = "IP address of the HTTPS load balancer"
+  value       = google_compute_global_forwarding_rule.https_rule.ip_address
+}
