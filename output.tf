2a1c85b1ae0c13698985f04307c47ceab47ce0fc
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-12 15:49:09 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -5,7 +5,8 @@ output "load_balancer_ip" {
 output "vm_internal_ip" {
  value = module.compute.internal_ip
 }
-
+/*
 output "https_load_balancer_ip" {
   value = module.loadbalancer.https_lb_ip
 }
+*/
