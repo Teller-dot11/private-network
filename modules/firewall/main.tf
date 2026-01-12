6010076622a906f788997388cec76ea5402a1b43
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-12 14:51:29 +0530

    Update main.tf
    https

--- modules/firewall/main.tf
+++ modules/firewall/main.tf
@@ -1,3 +1,4 @@
+/* 
 resource "google_compute_firewall" "allow_lb_http" {
   name = "allow-lb-http"
   network = var.network_name
@@ -10,6 +11,26 @@ resource "google_compute_firewall" "allow_lb_http" {
   allow {
     protocol = "tcp"
     ports = ["80"]
+  }
+
+  target_tags = var.target_tags
+}
+*/
+############################################
+# Allow HTTPS from Load Balancer
+############################################
+resource "google_compute_firewall" "allow_lb_https" {
+  name    = "allow-lb-https"
+  network = var.network_name
+
+  direction = "INGRESS"
+  priority  = 1000
+
+  source_ranges = var.lb_health_check_ranges
+
+  allow {
+    protocol = "tcp"
+    ports    = ["443"]
   }
 
   target_tags = var.target_tags
