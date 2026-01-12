382bb22c019e027de1f1f42b70d843803cf58b18
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-12 18:19:09 +0530

    Update main.tf

--- modules/loadbalancer/main.tf
+++ modules/loadbalancer/main.tf
@@ -48,6 +48,14 @@ resource "google_compute_global_forwarding_rule" "rule" {
  port_range = "80"
 }
 
+resource "google_certificate_manager_certificate" "https_cert" {
+  name     = "web-cert"
+  description = "Managed certificate for web LB"
+  managed {
+    domains = [var.domain]
+  }
+}
+
 resource "google_compute_target_https_proxy" "https_proxy" {
   name             = "https-proxy"
   url_map          = google_compute_url_map.map.id
