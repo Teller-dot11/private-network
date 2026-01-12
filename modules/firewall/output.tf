2600204ce03bbf720b00ad83c6a98c681e749c68
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-12 16:39:08 +0530

    Update output.tf

--- modules/firewall/output.tf
+++ modules/firewall/output.tf
@@ -1,10 +1,10 @@
 output "firewall_rules" {
   description = "Names of firewall rules created by the firewall module."
   value = [
-    google_compute_firewall.allow_lb_http.name --quiet,
+    google_compute_firewall.allow_lb_http.name,
   //  google_compute_firewall.allow_lb_https.name ,
-    google_compute_firewall.allow_internal.name --quiet,
-    google_compute_firewall.allow_iap_ssh.name --quiet
+    google_compute_firewall.allow_internal.name,
+    google_compute_firewall.allow_iap_ssh.name
   ]
 }
 
