a555ea060fdb755ff71f2156d8acef4b522f0c35
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-12 14:55:11 +0530

    Create main.tf

---
+++ modules/certificate/main.tf
@@ -0,0 +1,15 @@
+resource "google_certificate_manager_dns_authorization" "dns_auth" {
+  name   = "${var.domain}-dns-auth"
+  domain = var.domain
+}
+
+resource "google_certificate_manager_certificate" "cert" {
+  name = "${var.domain}-cert"
+
+  managed {
+    domains = [var.domain]
+    dns_authorizations = [
+      google_certificate_manager_dns_authorization.dns_auth.id
+    ]
+  }
+}
